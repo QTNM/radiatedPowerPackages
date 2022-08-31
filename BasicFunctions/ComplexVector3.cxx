1e99bb85941f603b6d14c5695505ff1488251786
Seb Jones <jonesseb100@gmail.com>
2022-08-31 15:18:34 +0100

    Filled out class for complex three vectors

--- BasicFunctions/ComplexVector3.cxx
+++ BasicFunctions/ComplexVector3.cxx
@@ -1,3 +1,55 @@
 /// ComplexVector3.cxx
 
 #include "BasicFunctions/ComplexVector3.h"
+
+#include <complex>
+
+#include "TVector3.h"
+
+std::complex<double> rad::ComplexVector3::Dot(ComplexVector3 vec)
+{
+  std::complex<double> sum{ mX*vec.X() + mY*vec.Y() + mZ*vec.Z() };
+  return sum;
+}
+
+rad::ComplexVector3 rad::ComplexVector3::Cross(ComplexVector3 vec)
+{
+  ComplexVector3 product{ mY*vec.Z()-vec.Y()*mZ, mZ*vec.X()-vec.Z()*mX, mX*vec.Y()-vec.X()*mY };
+  return product;
+}
+
+TVector3 rad::ComplexVector3::Real()
+{
+  return TVector3{ mX.real(), mY.real(), mZ.real() };
+}
+
+TVector3 rad::ComplexVector3::Imag()
+{
+  return TVector3{ mX.imag(), mY.imag(), mZ.imag() };
+}
+
+rad::ComplexVector3 rad::ComplexVector3::Conj()
+{
+  return ComplexVector3{ std::conj(mX), std::conj(mY), std::conj(mZ) };
+}
+
+rad::ComplexVector3 rad::ComplexVector3::operator + (ComplexVector3 const &vec)
+{
+  return ComplexVector3{ mX+vec.mX, mY+vec.mY, mZ+vec.mZ };
+}
+
+rad::ComplexVector3 rad::ComplexVector3::operator - (ComplexVector3 const &vec)
+{
+  return ComplexVector3{ mX-vec.mX, mY-vec.mY, mZ-vec.mZ };
+}
+
+rad::ComplexVector3 rad::ComplexVector3::operator - () const
+{
+  return ComplexVector3{ -mX, -mY, -mZ };
+}
+
+rad::ComplexVector3 rad::ComplexVector3::operator * (double a)
+{
+  return ComplexVector3{ a*mX, a*mY, a*mZ };
+}
+
--- BasicFunctions/ComplexVector3.h
+++ BasicFunctions/ComplexVector3.h
@@ -9,6 +9,8 @@
 
 #include <complex>
 
+#include "TVector3.h"
+
 namespace rad {
 
   class ComplexVector3 {
@@ -20,6 +22,47 @@ namespace rad {
   public:
     /// Parametrised constructor taking three complex numbers
     ComplexVector3(std::complex<double> x, std::complex<double> y, std::complex<double> z) : mX{x}, mY{y}, mZ{z} {}
+
+    /// Functions to return individual components
+    std::complex<double> X() { return mX; }
+    std::complex<double> Y() { return mY; }
+    std::complex<double> Z() { return mZ; }
+
+    /// Functions to set components after creation
+    void SetX(std::complex<double> x) { mX = x; }
+    void SetY(std::complex<double> y) { mY = y; }
+    void SetZ(std::complex<double> z) { mZ = z; }
+
+    /// Scalar product
+    /// \param vec The vector with which to compute the scalar product
+    /// \Returns The complex scalar product of the two vectors
+    std::complex<double> Dot(ComplexVector3 vec);
+
+    /// Vector product
+    /// \param vec The vector with which to compute the vector product
+    /// \Returns The complex vector product of the two vectors
+    ComplexVector3 Cross(ComplexVector3 vec);
+
+    /// Real component
+    TVector3 Real();
+
+    /// Imaginary component
+    TVector3 Imag();
+
+    /// Return the complex conjugate of the vector
+    ComplexVector3 Conj();
+
+    /// Addition of vectors
+    ComplexVector3 operator + (ComplexVector3 const &vec);
+
+    /// Subtraction of vectors
+    ComplexVector3 operator - (ComplexVector3 const &vec);
+
+    /// Unary minus
+    ComplexVector3 operator - () const;
+
+    /// Multiplication with a real number
+    ComplexVector3 operator * (double a);
   };
 }
 
