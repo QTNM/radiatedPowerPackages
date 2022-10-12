5810c207eb5ba6e11f20f15e8e97c0a6f3da5572
Seb Jones <jonesseb100@gmail.com>
2022-10-12 11:18:37 +0100

    Experimentation with VSCode auto-indent

--- EventGeneration/Event.cxx
+++ EventGeneration/Event.cxx
@@ -13,45 +13,47 @@
 #include <cassert>
 #include <tuple>
 
-rad::Event::Event(std::vector<ParticleState> particles, BaseField* field, double simStepSize, double simTime)
+rad::Event::Event(std::vector<ParticleState> particles, BaseField *field, double simStepSize, double simTime)
 {
   assert(particles.size() > 0);
-  
-  particleList          = particles;
-  magneticField         = field;
-  simulationStepSize    = simStepSize;
+
+  particleList = particles;
+  magneticField = field;
+  simulationStepSize = simStepSize;
   maximumSimulationTime = simTime;
   antennaList = {};
-  
+
   clockTime = 0.0; // Just for simplicity
 
   // Create a separate Boris solver for each particle
   // keep them in the same ordering as the particles
   solverList.clear();
-  for (auto & part : particleList) {
-    BorisSolver solver(field, part.GetParticleCharge(), part.GetParticleMass(), 2*R_E/(3*TMath::C()));
+  for (auto &part : particleList)
+  {
+    BorisSolver solver(field, part.GetParticleCharge(), part.GetParticleMass(), 2 * R_E / (3 * TMath::C()));
     solverList.push_back(solver);
   }
 }
 
-rad::Event::Event(std::vector<ParticleState> particles, std::vector<IAntenna*> antennas,
-		  BaseField* field, double simStepSize, double simTime)
+rad::Event::Event(std::vector<ParticleState> particles, std::vector<IAntenna *> antennas,
+                  BaseField *field, double simStepSize, double simTime)
 {
   assert(particles.size() > 0);
-  
-  particleList          = particles;
-  magneticField         = field;
-  simulationStepSize    = simStepSize;
+
+  particleList = particles;
+  magneticField = field;
+  simulationStepSize = simStepSize;
   maximumSimulationTime = simTime;
   antennaList = antennas;
-  
+
   clockTime = 0.0; // Just for simplicity
-  
+
   // Create a separate Boris solver for each particle
   // keep them in the same ordering as the particles
   solverList.clear();
-  for (auto & part : particleList) {
-    BorisSolver solver(field, part.GetParticleCharge(), part.GetParticleMass(), 2*R_E/(3*TMath::C()));
+  for (auto &part : particleList)
+  {
+    BorisSolver solver(field, part.GetParticleCharge(), part.GetParticleMass(), 2 * R_E / (3 * TMath::C()));
     solverList.push_back(solver);
   }
 }
@@ -63,9 +65,9 @@ bool rad::Event::ParticleStartCheck(ParticleState part)
 
 void rad::Event::AdvanceParticleStep(int particleNumber)
 {
-  std::tuple<TVector3, TVector3> outputVectors = solverList[particleNumber].advance_step(clockTime-particleList[particleNumber].currentTime, particleList[particleNumber].GetPositionVector(), particleList[particleNumber].GetVelocityVector());
-  
-  // Update the relevant stats 
+  std::tuple<TVector3, TVector3> outputVectors = solverList[particleNumber].advance_step(clockTime - particleList[particleNumber].currentTime, particleList[particleNumber].GetPositionVector(), particleList[particleNumber].GetVelocityVector());
+
+  // Update the relevant stats
   particleList[particleNumber].positionVector = std::get<0>(outputVectors);
   particleList[particleNumber].velocityVector = std::get<1>(outputVectors);
   particleList[particleNumber].currentTime += simulationStepSize;
@@ -73,19 +75,22 @@ void rad::Event::AdvanceParticleStep(int particleNumber)
 
 void rad::Event::PropagateParticles()
 {
-  std::cout<<"Simulation time is "<<maximumSimulationTime<<" s"<<std::endl;
-  std::cout<<"Simulation step size is "<<simulationStepSize<<" s"<<std::endl;
+  std::cout << "Simulation time is " << maximumSimulationTime << " s" << std::endl;
+  std::cout << "Simulation step size is " << simulationStepSize << " s" << std::endl;
   int nTimeSteps = maximumSimulationTime / simulationStepSize;
-  std::cout<<"Time steps = "<<nTimeSteps<<std::endl;
+  std::cout << "Time steps = " << nTimeSteps << std::endl;
 
   // Move forward through time
-  for (int iStep = 0; iStep < nTimeSteps; iStep++) {
-    clockTime += simulationStepSize;    
-    for (int iPart = 0; iPart < particleList.size(); iPart++) {
+  for (int iStep = 0; iStep < nTimeSteps; iStep++)
+  {
+    clockTime += simulationStepSize;
+    for (int iPart = 0; iPart < particleList.size(); iPart++)
+    {
       // Check if this particle is meant to be active yet
-      if (ParticleStartCheck(particleList[iPart])) {
-	      particleList[iPart].currentTime = clockTime;
-	      AdvanceParticleStep(iPart);
+      if (ParticleStartCheck(particleList[iPart]))
+      {
+        particleList[iPart].currentTime = clockTime;
+        AdvanceParticleStep(iPart);
       }
     }
   }
@@ -93,18 +98,20 @@ void rad::Event::PropagateParticles()
 
 rad::ParticleState rad::Event::GetParticle(int particleIndex)
 {
-  if (particleIndex >= 0 && particleIndex < particleList.size()) {
+  if (particleIndex >= 0 && particleIndex < particleList.size())
+  {
     return particleList[particleIndex];
   }
-  else {
+  else
+  {
     // Return a placeholder state so the application doesn't just crash
-    std::cout<<"Requested a particle that does not exist!"<<std::endl;
+    std::cout << "Requested a particle that does not exist!" << std::endl;
     ParticleState placeholder(clockTime, 0.0, 0.0, TVector3(9999.9, 9999.9, 9999.9), TVector3(0.0, 0.0, 0.0));
     return placeholder;
   }
 }
 
-double rad::Event::GetPropagationTime(ParticleState particle, IAntenna* antenna)
+double rad::Event::GetPropagationTime(ParticleState particle, IAntenna *antenna)
 {
   TVector3 disp = particle.GetPositionVector() - antenna->GetAntennaPosition();
   return (disp.Mag() / TMath::C());
@@ -112,5 +119,5 @@ double rad::Event::GetPropagationTime(ParticleState particle, IAntenna* antenna)
 
 unsigned int rad::Event::GetNParticles()
 {
-  return particleList.size(); 
+  return particleList.size();
 }
