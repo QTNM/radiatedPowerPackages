7a7bf1c2a435157c18d624397f427db4e35a8f65
Seb Jones <jonesseb100@gmail.com>
2023-03-23 14:24:49 +0000

    Whitespace changes

--- Antennas/IAntenna.cxx
+++ Antennas/IAntenna.cxx
@@ -1,13 +1,13 @@
 // IAntenna.cxx
 
+#include "Antennas/IAntenna.h"
+
 #include <cassert>
 #include <iostream>
 
-#include "Antennas/IAntenna.h"
 #include "BasicFunctions/BasicFunctions.h"
-
-#include "TVector3.h"
 #include "TMath.h"
+#include "TVector3.h"
 
 double rad::IAntenna::GetCentralWavelength() {
   double freq = GetCentralFrequency();
@@ -23,46 +23,32 @@ TVector3 rad::IAntenna::GetThetaHat(const TVector3 electronPosition) {
   double theta{0};
   double phi{0};
 
-  // Do theta first 
-  if (z > 0)
-  {
+  // Do theta first
+  if (z > 0) {
     theta = atan(sqrt(x * x + y * y) / z);
-  }
-  else if (z < 0)
-  {
+  } else if (z < 0) {
     theta = atan(sqrt(x * x + y * y) / z) + TMath::Pi();
-  }
-  else 
-  {
+  } else {
     theta = TMath::Pi() / 2;
   }
 
   // Now do phi
-  if (x > 0)
-  {
+  if (x > 0) {
     phi = atan(y / x);
-  }
-  else if (x < 0 && y >= 0)
-  {
+  } else if (x < 0 && y >= 0) {
     phi = atan(y / x) + TMath::Pi();
-  }
-  else if (x < 0 && y < 0)
-  {
+  } else if (x < 0 && y < 0) {
     phi = atan(y / x) - TMath::Pi();
-  }
-  else if (x == 0 && y > 0)
-  {
+  } else if (x == 0 && y > 0) {
     phi = TMath::Pi() / 2;
-  }
-  else 
-  {
-    phi = - TMath::Pi() / 2;
+  } else {
+    phi = -TMath::Pi() / 2;
   }
 
-  TVector3 vec(TMath::Cos(theta)*TMath::Cos(phi), 
-               TMath::Cos(theta)*TMath::Sin(phi), -1*TMath::Sin(theta));
-  TVector3 rotateVec{RotateToGlobalCoords(vec, antennaXAxis, antennaYAxis, 
-                                          antennaZAxis)};
+  TVector3 vec(TMath::Cos(theta) * TMath::Cos(phi),
+               TMath::Cos(theta) * TMath::Sin(phi), -1 * TMath::Sin(theta));
+  TVector3 rotateVec{
+      RotateToGlobalCoords(vec, antennaXAxis, antennaYAxis, antennaZAxis)};
   return rotateVec;
 }
 
@@ -71,30 +57,21 @@ TVector3 rad::IAntenna::GetPhiHat(const TVector3 electronPosition) {
   double x{antennaXAxis.Dot(rHat)};
   double y{antennaYAxis.Dot(rHat)};
   double phi{0};
-  if (x > 0)
-  {
+  if (x > 0) {
     phi = atan(y / x);
-  }
-  else if (x < 0 && y >= 0)
-  {
+  } else if (x < 0 && y >= 0) {
     phi = atan(y / x) + TMath::Pi();
-  }
-  else if (x < 0 && y < 0)
-  {
+  } else if (x < 0 && y < 0) {
     phi = atan(y / x) - TMath::Pi();
-  }
-  else if (x == 0 && y > 0)
-  {
+  } else if (x == 0 && y > 0) {
     phi = TMath::Pi() / 2;
-  }
-  else 
-  {
-    phi = - TMath::Pi() / 2;
+  } else {
+    phi = -TMath::Pi() / 2;
   }
 
-  TVector3 vec(-1*TMath::Sin(phi), TMath::Cos(phi), 0);
-  TVector3 rotateVec{RotateToGlobalCoords(vec, antennaXAxis, antennaYAxis, 
-                                          antennaZAxis)};
+  TVector3 vec(-1 * TMath::Sin(phi), TMath::Cos(phi), 0);
+  TVector3 rotateVec{
+      RotateToGlobalCoords(vec, antennaXAxis, antennaYAxis, antennaZAxis)};
   return rotateVec;
 }
 
@@ -111,34 +88,33 @@ double rad::IAntenna::GetPhi(const TVector3 electronPosition) {
   return phi;
 }
 
-void rad::IAntenna::SetBandwidth(const double lowerLimit, const double upperLimit) {
+void rad::IAntenna::SetBandwidth(const double lowerLimit,
+                                 const double upperLimit) {
   assert(lowerLimit < upperLimit);
   lowerBandwidth = lowerLimit;
   upperBandwidth = upperLimit;
 }
 
-double rad::IAntenna::GetPatternIntegral()
-{
+double rad::IAntenna::GetPatternIntegral() {
   // Now calculate the surface integral of the radiation pattern
   // Need this for proper calculation of the gain
   const int nPntsTheta{200};
   const int nPntsPhi{200};
   // Hypothetical width for integration
-  const double binArea{TMath::Pi() * 2 * TMath::Pi() / double(nPntsPhi * nPntsTheta)}; 
+  const double binArea{TMath::Pi() * 2 * TMath::Pi() /
+                       double(nPntsPhi * nPntsTheta)};
   const double binWidthTheta{TMath::Pi() / double(nPntsTheta)};
   const double binWidthPhi{2 * TMath::Pi() / double(nPntsPhi)};
   double PRad{0};
-  for (int ith{0}; ith < nPntsTheta; ith++)
-  {
+  for (int ith{0}; ith < nPntsTheta; ith++) {
     double theta{double(ith) * binWidthTheta + binWidthTheta / 2};
-    for (int iph{0}; iph < nPntsPhi; iph++)
-    {
+    for (int iph{0}; iph < nPntsPhi; iph++) {
       double phi{double(iph) * binWidthPhi + binWidthPhi / 2};
-      double uSin{(GetETheta(theta, phi) * GetETheta(theta, phi) + 
-                  GetEPhi(theta, phi) * GetEPhi(theta, phi)) * sin(theta)};
+      double uSin{(GetETheta(theta, phi) * GetETheta(theta, phi) +
+                   GetEPhi(theta, phi) * GetEPhi(theta, phi)) *
+                  sin(theta)};
       PRad += uSin * binArea;
     }
   }
   return PRad;
 }
-
--- Antennas/IAntenna.h
+++ Antennas/IAntenna.h
@@ -10,84 +10,86 @@
 
 namespace rad {
 
-  class IAntenna {
-    
-  public:
-    ~IAntenna(){}
-    
-    // Get the radiation patterns
-    // Overidden in each antenna derived class
-    virtual TVector3 GetETheta(const TVector3 electronPosition) = 0;
-    virtual TVector3 GetEPhi(const TVector3 electronPosition) = 0;
+class IAntenna {
+ public:
+  ~IAntenna() {}
 
-    /// \param electronPosition The position of the electron in global coordinates
-    /// \returns The polar angle theta w.r.t. the antenna axis
-    double GetTheta(const TVector3 electronPosition);
+  // Get the radiation patterns
+  // Overidden in each antenna derived class
+  virtual TVector3 GetETheta(const TVector3 electronPosition) = 0;
+  virtual TVector3 GetEPhi(const TVector3 electronPosition) = 0;
 
-    /// \param electronPosition The position of the electron in global coordinates
-    /// \returns The azimuthal angle phi w.r.t. the antenna axis
-    double GetPhi(const TVector3 electronPosition);
+  /// \param electronPosition The position of the electron in global coordinates
+  /// \returns The polar angle theta w.r.t. the antenna axis
+  double GetTheta(const TVector3 electronPosition);
 
-    // Radiation patterns taking angles as arguments
-    virtual double GetETheta(double theta, double phi) = 0;
-    virtual double GetEPhi(double theta, double phi) = 0;
+  /// \param electronPosition The position of the electron in global coordinates
+  /// \returns The azimuthal angle phi w.r.t. the antenna axis
+  double GetPhi(const TVector3 electronPosition);
 
-    // Returns the effective antenna height/length  
-    virtual double GetHEff() = 0;
+  // Radiation patterns taking angles as arguments
+  virtual double GetETheta(double theta, double phi) = 0;
+  virtual double GetEPhi(double theta, double phi) = 0;
 
-    virtual double GetHEff(TVector3 ePos) = 0;
+  // Returns the effective antenna height/length
+  virtual double GetHEff() = 0;
 
-    /// Virtual function for returning antenna effective area
-    /// \param electronPosition The position vector of the electron
-    /// \return The effective area of the antenna (in metres)
-    virtual double GetAEff(TVector3 electronPosition) = 0;
-    
-    double GetCentralFrequency(){ return centralFreq; }
-    
-    double GetCentralWavelength();
+  virtual double GetHEff(TVector3 ePos) = 0;
 
-    TVector3 GetAntennaPosition(){ return antennaPosition; }
+  /// Virtual function for returning antenna effective area
+  /// \param electronPosition The position vector of the electron
+  /// \return The effective area of the antenna (in metres)
+  virtual double GetAEff(TVector3 electronPosition) = 0;
 
-    double GetBandwidthUpperLimit() { return upperBandwidth; }
+  double GetCentralFrequency() { return centralFreq; }
 
-    double GetBandwidthLowerLimit() { return lowerBandwidth; }
+  double GetCentralWavelength();
 
-    void SetBandwidth(const double lowerLimit=-DBL_MAX, const double upperLimit=DBL_MAX);
+  TVector3 GetAntennaPosition() { return antennaPosition; }
 
-    double GetTimeDelay() { return timeDelay; }
+  double GetBandwidthUpperLimit() { return upperBandwidth; }
 
-    virtual double GetAEffTheta(TVector3 ePos) = 0;
+  double GetBandwidthLowerLimit() { return lowerBandwidth; }
 
-    virtual double GetAEffPhi(TVector3 ePos) = 0;
-    
-  protected:
-    TVector3 antennaPosition;
-    TVector3 antennaXAxis;
-    TVector3 antennaYAxis;
-    TVector3 antennaZAxis;
-    
-    /// The upper and lower frequency bandwidths 
-    double lowerBandwidth;
-    double upperBandwidth;
-    
-    /// Central frequency of the antenna bandwidth
-    double centralFreq;
+  void SetBandwidth(const double lowerLimit = -DBL_MAX,
+                    const double upperLimit = DBL_MAX);
 
-    /// Time delay for this antenna
-    double timeDelay; 
-    
-    /// \param electronPosition The position of the electron in global coordinates
-    /// \returns The unit vector in the theta direction (relative to antenna axis) in global coords
-    TVector3 GetThetaHat(const TVector3 electronPosition);
+  double GetTimeDelay() { return timeDelay; }
 
-    /// \param electronPosition The position of the electron in global coordinates
-    /// \returns The unit vector in the phi direction (relative to antenna axis) in global coords
-    TVector3 GetPhiHat(const TVector3 electronPosition);
+  virtual double GetAEffTheta(TVector3 ePos) = 0;
 
-    /// Gets the integral of the radiation pattern. Used for normalisation
-    double GetPatternIntegral();
-  };
+  virtual double GetAEffPhi(TVector3 ePos) = 0;
 
-}
+ protected:
+  TVector3 antennaPosition;
+  TVector3 antennaXAxis;
+  TVector3 antennaYAxis;
+  TVector3 antennaZAxis;
+
+  /// The upper and lower frequency bandwidths
+  double lowerBandwidth;
+  double upperBandwidth;
+
+  /// Central frequency of the antenna bandwidth
+  double centralFreq;
+
+  /// Time delay for this antenna
+  double timeDelay;
+
+  /// \param electronPosition The position of the electron in global coordinates
+  /// \returns The unit vector in the theta direction (relative to antenna axis)
+  /// in global coords
+  TVector3 GetThetaHat(const TVector3 electronPosition);
+
+  /// \param electronPosition The position of the electron in global coordinates
+  /// \returns The unit vector in the phi direction (relative to antenna axis)
+  /// in global coords
+  TVector3 GetPhiHat(const TVector3 electronPosition);
+
+  /// Gets the integral of the radiation pattern. Used for normalisation
+  double GetPatternIntegral();
+};
+
+}  // namespace rad
 
 #endif
