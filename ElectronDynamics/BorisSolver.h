ed6772af679d796203e5b7acd552813c6672941f
Seb Jones <jonesseb100@gmail.com>
2023-09-06 11:56:56 +0100

    Documentation

--- ElectronDynamics/BorisSolver.h
+++ ElectronDynamics/BorisSolver.h
@@ -26,14 +26,14 @@ class BorisSolver {
   BaseField* field = 0;
   CircularCavity* cav = 0;
 
-  /// Calculate omega as a function of position
-  /// \param pos Position of charge
+  /// @brief Calculate omega as a function of position
+  /// @param pos Position of charge
   TVector3 get_omega(const TVector3 pos);
 
-  /// Calculate radiation acceleration from position and velocity
-  /// \param pos Position of the charge
-  /// \param vec Velocity of the charge
-  /// \Returns a 3-vector of the acceleration from the RR force
+  /// @brief Calculate radiation acceleration from position and velocity
+  /// @param pos Position of the charge
+  /// @param vec Velocity of the charge
+  /// @return a 3-vector of the acceleration from the RR force
   TVector3 radiation_acceleration(const TVector3 pos, const TVector3 vel);
 
   /// @brief Returns the B field at the position
@@ -47,32 +47,34 @@ class BorisSolver {
   TVector3 calc_e_field(TVector3 pos);
 
  public:
-  /// Default constructor
+  /// @brief Default constructor
   /// By default a uniform field is used
   BorisSolver();
 
-  /// \param field_v Pointer to a magnetic field calculator
-  /// \param charge_v Particle charge. Default is electron charge
-  /// \param mass_v Particle mass. Default is electron mass
-  /// \param tau_v Energy loss. Default is zero
+  /// @brief Parametrised constructor
+  /// @param field_v Pointer to a magnetic field calculator
+  /// @param charge_v Particle charge. Default is electron charge
+  /// @param mass_v Particle mass. Default is electron mass
+  /// @param tau_v Energy loss. Default is zero
+  /// @param cavity Pointer to resonant cavity (if using)
   BorisSolver(BaseField* field_v, const double charge_v = -TMath::Qe(),
               const double mass_v = ME, const double tau_v = 0.0,
               CircularCavity* cavity = 0);
 
-  /// Advances position and velocity vector by a set time
-  /// \param time_step The time step over which to advance the charge's motion
-  /// \param x0 Vector of the charge's starting position
-  /// \param v0 Vector of the charge's starting velocity
-  /// \returns Tuple containing (1) the output position vector and (2) the
+  /// @brief Advances position and velocity vector by a set time
+  /// @param time_step The time step over which to advance the charge's motion
+  /// @param x0 Vector of the charge's starting position
+  /// @param v0 Vector of the charge's starting velocity
+  /// @return Tuple containing (1) the output position vector and (2) the
   /// output velocity vector
   std::tuple<TVector3, TVector3> advance_step(const double time_step,
                                               const TVector3 x0,
                                               const TVector3 v0);
 
-  /// Calculate acceleration due to B field and RR force
-  /// \param pos Position of the charge
-  /// \param vec Velocity of the charge
-  /// \returns a 3-vector of the acceleration
+  /// @brief acceleration due to B field and RR force
+  /// @param pos Position of the charge
+  /// @param vec Velocity of the charge
+  /// @return a 3-vector of the acceleration
   TVector3 acc(const TVector3 pos, const TVector3 vel);
 };
 }  // namespace rad
