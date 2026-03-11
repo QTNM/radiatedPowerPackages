22fb492aa7e16949c776478a7a17330000a4397c
Seb Jones <jonesseb100@gmail.com>
2026-03-11 17:04:19 +0000

    Throw exceptions in the case of invalid graphs

--- utilities/ROOTUtils/FFTAnalysis.cxx
+++ utilities/ROOTUtils/FFTAnalysis.cxx
@@ -1,12 +1,15 @@
-/// FFTAnalysis.cxx - ROOT-based FFT and frequency domain analysis (using SignalUtils internally)
+/// FFTAnalysis.cxx - ROOT-based FFT and frequency domain analysis (using
+/// SignalUtils internally)
 #include "utilities/ROOTUtils/FFTAnalysis.h"
 
+#include <cmath>
+#include <stdexcept>
+
+#include "TF1.h"
+#include "TRandom3.h"
 #include "utilities/BasicCore/Constants.h"
 #include "utilities/ROOTUtils/GraphUtils.h"
 #include "utilities/SignalUtils/SpectrumAnalysis.h"
-#include <cmath>
-#include "TF1.h"
-#include "TRandom3.h"
 
 using namespace rad;
 
@@ -15,14 +18,21 @@ TGraph *rad::MakePowerSpectrumNorm(const TGraph *grWave) {
   double *timeData = grWave->GetY();
   double *xData = grWave->GetX();
   int n = grWave->GetN();
+
+  if (n < 2) {
+    throw std::invalid_argument(
+        "Input graphs requires at least two points to compute spectrum");
+  }
+
   double deltaT = xData[1] - xData[0];
-  
+
   // Convert to vector and process using SignalUtils
   std::vector<double> timeVec(timeData, timeData + n);
   auto spectrum = rad::MakePowerSpectrumNorm(timeVec, deltaT);
-  
+
   // Convert back to TGraph
-  TGraph *result = new TGraph(spectrum.first.size(), spectrum.first.data(), spectrum.second.data());
+  TGraph *result = new TGraph(spectrum.first.size(), spectrum.first.data(),
+                              spectrum.second.data());
   setGraphAttr(result);
   return result;
 }
@@ -32,14 +42,21 @@ TGraph *rad::MakePowerSpectrumPeriodogram(const TGraph *grWave) {
   double *timeData = grWave->GetY();
   double *xData = grWave->GetX();
   int n = grWave->GetN();
+
+  if (n < 2) {
+    throw std::invalid_argument(
+        "Input graph requires at least two points to compute spectrum");
+  }
+
   double deltaT = xData[1] - xData[0];
-  
+
   // Convert to vector and process using SignalUtils
   std::vector<double> timeVec(timeData, timeData + n);
   auto spectrum = rad::MakePowerSpectrumPeriodogram(timeVec, deltaT);
-  
+
   // Convert back to TGraph
-  TGraph *result = new TGraph(spectrum.first.size(), spectrum.first.data(), spectrum.second.data());
+  TGraph *result = new TGraph(spectrum.first.size(), spectrum.first.data(),
+                              spectrum.second.data());
   setGraphAttr(result);
   result->GetXaxis()->SetTitle("Frequency [Hz]");
   return result;
@@ -50,14 +67,21 @@ TGraph rad::MakePowerSpectrumPeriodogram(const TGraph &grWave) {
   double *timeData = grWave.GetY();
   double *xData = grWave.GetX();
   int n = grWave.GetN();
+
+  if (n < 2) {
+    throw std::invalid_argument(
+        "Input graph requires at least two points to compute spectrum");
+  }
+
   double deltaT = xData[1] - xData[0];
-  
+
   // Convert to vector and process using SignalUtils
   std::vector<double> timeVec(timeData, timeData + n);
   auto spectrum = rad::MakePowerSpectrumPeriodogram(timeVec, deltaT);
-  
+
   // Convert back to TGraph
-  TGraph result(spectrum.first.size(), spectrum.first.data(), spectrum.second.data());
+  TGraph result(spectrum.first.size(), spectrum.first.data(),
+                spectrum.second.data());
   setGraphAttr(result);
   result.GetXaxis()->SetTitle("Frequency [Hz]");
   return result;
@@ -69,15 +93,15 @@ double rad::IntegratePowerNorm(const TGraph *grFFT, Int_t firstBin,
   double *freqData = grFFT->GetX();
   double *powerData = grFFT->GetY();
   int n = grFFT->GetN();
-  
+
   // Handle default values
   if (firstBin < 0) firstBin = 0;
   if (lastBin < 0) lastBin = n - 1;
-  
+
   // Convert to vectors
   std::vector<double> freqVec(freqData, freqData + n);
   std::vector<double> powerVec(powerData, powerData + n);
-  
+
   return rad::IntegratePowerNorm(freqVec, powerVec, firstBin, lastBin);
 }
 
@@ -87,12 +111,19 @@ TGraph *rad::BandPassFilter(const TGraph *grWave, const double minFreq,
   double *timeData = grWave->GetY();
   double *xData = grWave->GetX();
   int n = grWave->GetN();
+
+  if (n < 2) {
+    throw std::invalid_argument(
+        "Input graph requires at least two points to compute spectrum");
+  }
+
   double deltaT = xData[1] - xData[0];
-  
+
   // Convert to vector and process using SignalUtils
   std::vector<double> timeVec(timeData, timeData + n);
-  auto filteredData = rad::BandPassFilterSpectrum(timeVec, deltaT, minFreq, maxFreq);
-  
+  auto filteredData =
+      rad::BandPassFilterSpectrum(timeVec, deltaT, minFreq, maxFreq);
+
   // Convert back to TGraph
   return new TGraph(n, xData, filteredData.data());
 }
@@ -102,14 +133,21 @@ TGraph *rad::MakeFFTMagGraph(TGraph *grInput) {
   double *timeData = grInput->GetY();
   double *xData = grInput->GetX();
   int n = grInput->GetN();
+
+  if (n < 2) {
+    throw std::invalid_argument(
+        "Input graph requires at least two points to compute spectrum");
+  }
+
   double deltaT = xData[1] - xData[0];
-  
+
   // Convert to vector and process using SignalUtils
   std::vector<double> timeVec(timeData, timeData + n);
   auto spectrum = rad::MakeFFTMagnitude(timeVec, deltaT);
-  
+
   // Convert back to TGraph
-  TGraph *result = new TGraph(spectrum.first.size(), spectrum.first.data(), spectrum.second.data());
+  TGraph *result = new TGraph(spectrum.first.size(), spectrum.first.data(),
+                              spectrum.second.data());
   setGraphAttr(result);
   return result;
 }
@@ -128,12 +166,18 @@ void rad::AddWhiteNoiseFrequencyDomainPowerNorm(TGraph *grIn, const double Teff,
   // Extract data from TGraph
   double *powerData = grIn->GetY();
   int n = grIn->GetN();
+
+  if (n < 2) {
+    throw std::invalid_argument(
+        "Input graph requires at least two points to compute spectrum");
+  }
+
   double deltaF = grIn->GetPointX(1) - grIn->GetPointX(0);
-  
+
   // Convert to vector and process using SignalUtils
   std::vector<double> powerVec(powerData, powerData + n);
   rad::AddWhiteNoiseFrequencyDomain(powerVec, deltaF, Teff, seed);
-  
+
   // Update TGraph with modified data
   for (int i = 0; i < n; i++) {
     grIn->SetPointY(i, powerVec[i]);
