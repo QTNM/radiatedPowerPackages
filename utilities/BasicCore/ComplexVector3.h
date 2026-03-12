24af0d3d1c9fb7b7ccb4761c15d2a7d84d71e230
Seb Jones <jonesseb100@gmail.com>
2026-03-12 10:50:48 +0000

    Remove dead code version of ComplexVector3

--- utilities/BasicCore/ComplexVector3.h
+++ utilities/BasicCore/ComplexVector3.h
@@ -5,8 +5,8 @@
   No external dependencies - pure C++ implementation
 */
 
-#ifndef BASIC_CORE_COMPLEX_VECTOR_THREE_H
-#define BASIC_CORE_COMPLEX_VECTOR_THREE_H
+#ifndef COMPLEX_VECTOR_THREE_H
+#define COMPLEX_VECTOR_THREE_H
 
 #include <array>
 #include <complex>
